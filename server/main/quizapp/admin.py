267bd53a103dca2beff788f95fe6e083345d58ac
stephenbowen <bowensteven17@gmail.com>
2024-10-11 11:47:39 +0300

    updates on main

--- server/main/quizapp/admin.py
+++ server/main/quizapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Topic, DifficultyLevel, Question, Option, QuizSession, QuizSessionQuestion
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(DifficultyLevel)
+admin.site.register(Question)
+admin.site.register(Option)
+admin.site.register(QuizSession)
+admin.site.register(QuizSessionQuestion)
--- server/main/quizapp/views.py
+++ server/main/quizapp/views.py
@@ -7,16 +7,50 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.tokens import RefreshToken
+from .models import Question
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])  # Ensure access token is valid
 def logout_view(request):
     try:
-        refresh_token = request.data["refresh_token"]
-        print(f"Received refresh token: {refresh_token}") 
+        refresh_token = request.data.get("refresh_token")
+        if not refresh_token:
+            return Response({"error": "Refresh token is missing"}, status=status.HTTP_400_BAD_REQUEST)
+
+        print(f"Received refresh token: {refresh_token}")  # Log the received refresh token
         token = RefreshToken(refresh_token)
         token.blacklist()  # Blacklist the refresh token
 
         return Response(status=status.HTTP_205_RESET_CONTENT)  # Return 205 Reset Content
     except Exception as e:
-        return Response(status=status.HTTP_400_BAD_REQUEST)  # Handle errors
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET'])
+def get_questions(request):
+    questions = Question.objects.all().values()
+    question_list = list(questions)
+
+    return Response({"Questions":question_list},status=status.HTTP_200_OK )
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])  # Ensure that only authenticated users can access this route
+def get_user_details(request):
+    # Get the currently authenticated user
+    user = request.user
+    
+    # If the user is authenticated, return their details
+    if user:
+        user_data = {
+            "id": user.id,
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+        }
+        return Response(user_data, status=status.HTTP_200_OK)
+    
+    return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
